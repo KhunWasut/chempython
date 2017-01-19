d778f572ff2ba5c84efea4cc81f4e420a32ee3e7
Wasut Pornpatcharapong <wasut.pornpatcharapong@gmail.com>
2017-01-19 10:00:21 -0800

    [master 20170119 E1] 'chemmatrixaux.py' (Rev. 3) (see notes)
    - Added d_dx_pbc and new dx_pbc methods
    - These apply the new modular schemes to streamline the gradient and
      hessian codes for readability.

--- chemmatrixaux.py
+++ chemmatrixaux.py
@@ -19,7 +19,7 @@ number_to_axis = {0: 'x', 1: 'y', 2: 'z'}
 # Mapper
 def atomindex_to_vecindex(atom_a_index, axis):
    return (3 * (atom_a_index) + axis_to_number['axis'])
-\
+
 
 def vecindex_to_atomindex(vec_index_i):
    atom_a_index = int(vec_index_i / 3)
@@ -48,14 +48,51 @@ def index_verify_1d(vec_index_i, atom_a_index, atom_b_index):
       sys.exit(1)
 
 
-def dx_pbc(x_a_oneaxis, x_b_oneaxis, L):
-   dx_oneaxis = x_a_oneaxis - x_b_oneaxis
-   if (x_a_oneaxis - x_b_oneaxis) > L/2:
-      dx_oneaxis -= L
-   elif (x_a_oneaxis - x_b_oneaxis) < L/2:
-      dx_oneaxis += L
+def dx_pbc(x_a, x_b, vec_index_i, L):
+   # Calculate x_a - x_b given vec_index_i
+   # x_a = [x_ax, x_ay, x_az], similar to x_b
+   axis = vecindex_to_atomindex(vec_index_i)[1]
 
-   return dx_oneaxis
+   dx = x_a[axis] - x_b[axis]
+
+   if (x_a[axis] - x_b[axis]) > L/2:
+      dx -= L/2
+   elif (x_a[axis] - x_b[axis]) < -L/2:
+      dx += L/2
+
+   return dx
+
+
+def d_dx_pbc(atom_a_index, atom_b_index, vec_index_k, vec_index_l):
+   # Calculate d/dx_l (x_a - x_b)| axis(k)
+   # If l is not part of the domain, return 0
+   # If l is part of the domains,
+   # ...If vecindex_to_atomindex(l)[axis] = vecindex_to_atomindex(k)[axis]
+   # ......If vecindex_to_atomindex(k)[atom] = atom_a_index (can use index_verify_1d)
+   # .........return 1.0
+   # ......else if vecindex_to_atomindex(k)[atom] = atom_b_index
+   # .........return -1.0
+   # ......else
+   # .........return 0.0
+   # ...else (mismatch axis)
+   # ......return 0.0
+
+   domain_vec_indices = [atomindex_to_vecindex(atom_a_index,'x'), atomindex_to_vecindex(atom_a_index,'y'), atomindex_to_vecindex(atom_a_index,'z'),\
+         atomindex_to_vecindex(atom_b_index,'x'), atomindex_to_vecindex(atom_b_index,'y'), atomindex_to_vecindex(atom_b_index,'z')]
+
+   if not (vec_index_l in domain_vec_indices):
+      return 0.0
+   else:
+      # Check if axis in l and k indices are the same
+      if vecindex_to_atomindex(vec_index_l)[1] == vecindex_to_atomindex(vec_index_k)[1]:
+         if index_verify_1d(vec_index_k, atom_a_index, atom_b_index) == 'FT':
+            return 1.0
+         elif index_verify_1d(vec_index_k, atom_a_index, atom_b_index) == 'LT':
+            return -1.0
+         else:
+            return 0.0
+      else:
+         return 0.0
 
 
 def dx_ab(X_m, vec_index_i, atom_a_index, atom_b_index, L):
