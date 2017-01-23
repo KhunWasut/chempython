aa32ce5f8e245557e23d63ed4308a6c96f3a3f86
Wasut Pornpatcharapong <wasut.pornpatcharapong@gmail.com>
2017-01-23 13:45:13 -0800

    [master 20170123 E2] 'chemmatrix.py' (Rev. 1) (see notes)
    - Vector reading method

---
+++ chemmatrix.py
@@ -0,0 +1,12 @@
+#### chemmatrix.py ####
+
+import numpy as np
+import pandas as pd
+
+
+# This module reads matrices / vector from standard .vec files
+def read_vec(file_path):
+    # Implement check for .vec file in the future!
+    df = pd.read_csv(file_path, sep='\s+', header=None)
+    vec = df.as_matrix()
+    return vec
