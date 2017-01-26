ad83c4397a9fc756489577581e051de4568e11d2
Wasut Pornpatcharapong <wasut.pornpatcharapong@gmail.com>
2017-01-26 12:48:40 -0800

    [master 20170126 E2] 'chemmatrix.py' (Rev. 2) (see notes)
    - Fixed the pandas dataframe reading from vector files
    - Pandas read into 2D arrays, so fixed to returning 1D arrays instead.

--- chemmatrix.py
+++ chemmatrix.py
@@ -9,4 +9,6 @@ def read_vec(file_path):
     # Implement check for .vec file in the future!
     df = pd.read_csv(file_path, sep='\s+', header=None)
     vec = df.as_matrix()
+    # vec is a 2D array with 1 column. We want only 1D array!
+    vec = vec.reshape(vec.shape[0])
     return vec
