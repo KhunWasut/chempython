381e8b01d8490c5bde5f90dd08638ef499c5398d
Wasut Pornpatcharapong <wasut.pornpatcharapong@gmail.com>
2017-01-27 13:32:51 -0800

    [master 20170127 E1] 'utils.py' (Rev. 2) (see notes)
    - Added mass dictionary in utils.py, containing a.m.u. masses of Li, H,
      O, Cl for now

--- utils.py
+++ utils.py
@@ -1,6 +1,16 @@
 import re
 
 
+# Mass Dictionary (in a.m.u.)
+# for now, only list those relevant to our simulation
+masses = {
+    'H': 1.0079,
+    'Li': 6.941,
+    'O': 15.9994,
+    'Cl': 35.453
+}
+
+
 def atoi(text):
     return int(text) if text.isdigit() else text
 
