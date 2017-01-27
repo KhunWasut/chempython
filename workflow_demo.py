8317e3b12fb664740f431dabc282c6f724400e8e
Wasut Pornpatcharapong <wasut.pornpatcharapong@gmail.com>
2017-01-27 13:34:27 -0800

    [master 20170127 E2] 'workflow_demo.py' (Rev. 3) (see notes)
    - Edit scheme but not tested up to part 3 (ICF calculation)
    - Suggest that ICF calculation should be in atomic units
    - But convert back to NAMD energy unit after the ICF process is done for
      one-stop fit into kcal/mol and proper C.V. unit (A) for r_ab

--- workflow_demo.py
+++ workflow_demo.py
@@ -30,38 +30,46 @@ for i in range(len(x_snapshot_filelist)):
     X.append(vec_x)
     GRAD_V.append(vec_f)
 
-##### END OF PART 1 - MAKE SURE THIS PART WORKS BEFORE PROCEEDING! #####
+##### END OF PART 1 - CHECKED!! #####
 
 atom_vec = kp.chemmatrix.readvec('./aux/atoms.vec')
 
 masses = []
 # Build a mass matrix
 for atom in atom_vec:
-    if atom in kp.constants.atom_masses.keys:
-        masses.append(kp.constants.atom_mass[key])
+    if atom in kp.utils.masses:
+        masses.append(kp.utils.masses[atom])
 
+# This mass vector is in a.m.u.
 mass_vec = np.array(masses)
+# convert to atomic unit
+mass_vec_atomic = mass_vec*(1.66054e-27/9.10938e-31)
 mu = np.diag(mass_vec)
 
 ##### END OF PART 2 - MAKE SURE THIS PART WORKS BEFORE PROCEEDING! #####
 
-icf = []
+icf_atomic = []
 CN = kp.colvars.coordnum.CNParams(numer_pow=10.0, denom_pow=26.0, r0=3.2)
 R = kp.colvars.distance.RParams()
-L = 11.093
+L = 11.093 
+L_atomic = L/0.529177
 
 # Before calculations, make sure that distance c.v. is in atomic units!
+# Convert X into atomic unit
+X_atomic = ### Original unit: A
+GRAD_V_atomic = ### Original unit: kcal/(mol.A)
 
-for xi, grad_vi in X, GRAD_V:
+for xi, grad_vi in X_atomic, GRAD_V_atomic:
     icf_i = kp.icf.icf_construct(xi, mu, grad_vi, kT, R, CN, L)
     for icf_val in icf_i:
         icf.append(icf_val)
 
-icf = np.array(icf)
+icf_atomic = np.array(icf_atomic)
+icf_namd_unit = icf_atomic ### Convert back to NAMD units (either kcal/(mol.A) or kcal/(mol.A^2) for r_ab C.V.
 
 ##### END OF PART 3 - MAKE SURE THIS PART WORKS BEFORE PROCEEDING! #####
 
-icf_var = kp.icf.var(icf, num_cv=2)
+icf_var = kp.icf.var(icf_namd_unit, num_cv=2)
 
 ### Actually calculate CN and construct r_vec and cn_vec vectors
 ### Using what we have
