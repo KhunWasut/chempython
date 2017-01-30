b7180a589463ad0d09780af160dcc9e666c5ff75
Wasut Pornpatcharapong <wasut.pornpatcharapong@gmail.com>
2017-01-30 08:56:12 -0800

    [master 20170130 E2] 'colvars/params.py' (Rev. 1) (see notes)
    - Created this file to be used as colvars data structure

---
+++ colvars/params.py
@@ -0,0 +1,28 @@
+##### colvars/params.py #####
+
+# DATA STRUCTURES FOR COLVARS
+
+
+class RParams:
+    a_ind = 0
+    b_ind = 0
+
+    def __init__(self, a=0, b=0):
+        self.a_ind = a
+        self.b_ind = b
+
+
+class CNParams:
+    # Standard unit in this data structure: angstroms
+    a_ind = 0
+    b_inds = []
+    n = 0
+    m = 0
+    r0 = 0
+
+    def __init__(self, a=0, b_list=[], n=0, m=0, r0=0):
+        self.a_ind = a
+        self.b_inds = b_list
+        self.n = n
+        self.m = m
+        self.r0 = 0
