c4b48c3083aa9b43d1b9b83e97841eddcd7565ee
Wasut Pornpatcharapong <wasut.pornpatcharapong@gmail.com>
2017-01-31 08:12:54 -0800

    [master 20170131 E11] 'colvars/coordnum.py' (Rev. 8) (see notes)
    - The actual time to evaluate ICF is really slow due to the evaluation
      of the second derivative of C.N.
    - Improved the performance by bypassing evaluations when the term is
      surely zero.

--- colvars/coordnum.py
+++ colvars/coordnum.py
@@ -4,6 +4,7 @@ import numpy as np
 import sys, os
 
 from ..chemmatrixaux import atomindex_to_vecindex as atv
+from ..chemmatrixaux import d_dx_pbc
 
 from .distance import dr_dxi, d2r_dxjxi
 
@@ -105,32 +106,49 @@ def hess_x_j(X_m, vec_index_j, cn_params, L):
          x_b = np.array([X_m[atv(b,'x')], X_m[atv(b,'y')], X_m[atv(b,'z')]])
          r_ab = pair_dx(x_a, x_b, L)
 
-         D_val = D(r_ab, r0, m)
-         N_val = N(r_ab, r0, n)
-         dDj = dD_xi(m, r0, x_a, x_b, a, b, j, L, r_ab)
-         dNj = dN_xi(n, r0, x_a, x_b, a, b, j, L, r_ab)
-         d2r_ji = d2r_dxjxi(x_a, x_b, a, b, i, j, L, r_ab) 
-         dr_i = dr_dxi(x_a, x_b, a, b, i, L, r_ab)
-         dr_j = dr_dxi(x_a, x_b, a, b, j, L, r_ab)
+         # If derivatives evaluate to 0, simply bypass below calculations to save time!
+         if ((d_dx_pbc(a,b,i,i)==0.0) and (d_dx_pbc(a,b,i,j)==0.0) and (d_dx_pbc(a,b,j,j)==0.0)):
+            sum_d2cn_dxjxi = 0.0
+         else:
+            D_val = D(r_ab, r0, m)
+            N_val = N(r_ab, r0, n)
+            if d_dx_pbc(a,b,j,j) == 0.0:
+               dDj = 0.0
+               dNj = 0.0
+               dr_j = 0.0
+            else:
+               dDj = dD_xi(m, r0, x_a, x_b, a, b, j, L, r_ab)
+               dNj = dN_xi(n, r0, x_a, x_b, a, b, j, L, r_ab)
+               dr_j = dr_dxi(x_a, x_b, a, b, j, L, r_ab)
 
-         factor = D_val**(-4.0)
-         hess_t1_1 = dr_i*(n*D_val*dNj)/r_ab
-         hess_t1_2 = dr_i*(n*(N_val-1.0)*dDj)/r_ab
-         hess_t1_3 = n*D_val*(N_val - 1.0)*d2r_ji/r_ab
-         hess_t1_4 = (-1.0)*n*D_val*(N_val - 1.0)*dr_i*(dr_j/(r_ab*r_ab))
-         hess_t1 = (D_val * D_val) * (hess_t1_1 + hess_t1_2 + hess_t1_3 + hess_t1_4)
+            if d_dx_pbc(a,b,i,i) == 0.0:
+               dr_i = 0.0
+            else:
+               dr_i = dr_dxi(x_a, x_b, a, b, i, L, r_ab)
 
-         hess_t2_1 = dr_i*(m*N_val*dDj)/r_ab
-         hess_t2_2 = dr_i*(m*(D_val-1.0)*dNj)/r_ab
-         hess_t2_3 = m*N_val*(D_val - 1.0)*d2r_ji/r_ab
-         hess_t2_4 = (-1.0)*m*N_val*(D_val - 1.0)*dr_i*(dr_j/(r_ab*r_ab))
-         hess_t2 = (D_val * D_val) * (hess_t2_1 + hess_t2_2 + hess_t2_3 + hess_t2_4)
+            if ((d_dx_pbc(a,b,i,i) == 0.0) or ((d_dx_pbc(a,b,i,j)==0.0) and (d_dx_pbc(a,b,j,j)==0.0))):
+               d2r_ji = 0.0
+            else:
+               d2r_ji = d2r_dxjxi(x_a, x_b, a, b, i, j, L, r_ab)
 
-         hess_t3_1 = n*D_val*(N_val-1.0)*dr_i*dDj/r_ab
-         hess_t3_2 = m*N_val*(D_val-1.0)*dr_i*dDj/r_ab
-         hess_t3 = 2.0*D_val*(hess_t3_1 - hess_t3_2)
+            factor = D_val**(-4.0)
+            hess_t1_1 = dr_i*(n*D_val*dNj)/r_ab
+            hess_t1_2 = dr_i*(n*(N_val-1.0)*dDj)/r_ab
+            hess_t1_3 = n*D_val*(N_val - 1.0)*d2r_ji/r_ab
+            hess_t1_4 = (-1.0)*n*D_val*(N_val - 1.0)*dr_i*(dr_j/(r_ab*r_ab))
+            hess_t1 = (D_val * D_val) * (hess_t1_1 + hess_t1_2 + hess_t1_3 + hess_t1_4)
 
-         sum_d2cn_dxjxi += (factor*(hess_t1 - hess_t2 - hess_t3))
+            hess_t2_1 = dr_i*(m*N_val*dDj)/r_ab
+            hess_t2_2 = dr_i*(m*(D_val-1.0)*dNj)/r_ab
+            hess_t2_3 = m*N_val*(D_val - 1.0)*d2r_ji/r_ab
+            hess_t2_4 = (-1.0)*m*N_val*(D_val - 1.0)*dr_i*(dr_j/(r_ab*r_ab))
+            hess_t2 = (D_val * D_val) * (hess_t2_1 + hess_t2_2 + hess_t2_3 + hess_t2_4)
+
+            hess_t3_1 = n*D_val*(N_val-1.0)*dr_i*dDj/r_ab
+            hess_t3_2 = m*N_val*(D_val-1.0)*dr_i*dDj/r_ab
+            hess_t3 = 2.0*D_val*(hess_t3_1 - hess_t3_2)
+
+            sum_d2cn_dxjxi += (factor*(hess_t1 - hess_t2 - hess_t3))
 
       hess_x_j_list.append(sum_d2cn_dxjxi)
 
